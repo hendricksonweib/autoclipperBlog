d173c8b382d14dcc4e355e6b447c97d7eaf64ec2
hendricksonweib <hendricksonrocha22@gmail.com>
2024-03-16 18:26:35 -0300

    feat background
    Signed-off-by: hendricksonweib <hendricksonrocha22@gmail.com>

---
+++ src/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css'; 
+
+const Header = () => {
+  return (
+    <div className="Header">
+      <h1>Meu Cabeçalho</h1>
+      <p>Este é um exemplo de cabeçalho estilizado com CSS.</p>
+    </div>
+  );
+}
+
+export default Header;
