f3817ae95b0b07855de7a6dd91180d1a467a7f26
hendricksonweib <hendricksonrocha22@gmail.com>
2024-03-22 10:57:13 -0300

    feature Accordion session
    Signed-off-by: hendricksonweib <hendricksonrocha22@gmail.com>

--- src/Accordion.jsx
+++ src/Accordion.jsx
@@ -12,31 +12,31 @@ function MyAccordion() {
                 <div className="col-6 mt-4">
                     <BootstrapAccordion defaultActiveKey="0">
                         <BootstrapAccordion.Item eventKey="0">
-                            <BootstrapAccordion.Header>O que é a Autoclipper e como ela pode ajudar na entrega de conteúdo?</BootstrapAccordion.Header>
+                            <BootstrapAccordion.Header style={{ backgroundColor: 'black', color: 'white' }}>O que é a Autoclipper e como ela pode ajudar na entrega de conteúdo?</BootstrapAccordion.Header>
                             <BootstrapAccordion.Body>
                                 texto
                             </BootstrapAccordion.Body>
                         </BootstrapAccordion.Item>
                         <BootstrapAccordion.Item eventKey="1">
-                            <BootstrapAccordion.Header>Quais são os principais benefícios de usar a Autoclipper?</BootstrapAccordion.Header>
+                            <BootstrapAccordion.Header style={{ backgroundColor: 'black', color: 'white' }}>Quais são os principais benefícios de usar a Autoclipper?</BootstrapAccordion.Header>
                             <BootstrapAccordion.Body>
                                 texto
                             </BootstrapAccordion.Body>
                         </BootstrapAccordion.Item>
                         <BootstrapAccordion.Item eventKey="2">
-                            <BootstrapAccordion.Header>Para quem é destinado o serviço da Autoclipper?</BootstrapAccordion.Header>
+                            <BootstrapAccordion.Header style={{ backgroundColor: 'black', color: 'white' }}>Para quem é destinado o serviço da Autoclipper?</BootstrapAccordion.Header>
                             <BootstrapAccordion.Body>
                                 texto
                             </BootstrapAccordion.Body>
                         </BootstrapAccordion.Item>
                         <BootstrapAccordion.Item eventKey="3">
-                            <BootstrapAccordion.Header>Quais resultados posso esperar ao usar a Autoclipper?</BootstrapAccordion.Header>
+                            <BootstrapAccordion.Header style={{ backgroundColor: 'black', color: 'white' }}>Quais resultados posso esperar ao usar a Autoclipper?</BootstrapAccordion.Header>
                             <BootstrapAccordion.Body>
                                 texto
                             </BootstrapAccordion.Body>
                         </BootstrapAccordion.Item>
                         <BootstrapAccordion.Item eventKey="4">
-                            <BootstrapAccordion.Header>Quanto custa os serviços da Autoclipper?</BootstrapAccordion.Header>
+                            <BootstrapAccordion.Header style={{ backgroundColor: 'black', color: 'white' }}>Quanto custa os serviços da Autoclipper?</BootstrapAccordion.Header>
                             <BootstrapAccordion.Body>
                                 texto
                             </BootstrapAccordion.Body>
