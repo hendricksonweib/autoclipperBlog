159f8614132e11ff63371056cfee86b84d6720a2
hendricksonweib <hendricksonrocha22@gmail.com>
2024-03-24 18:49:38 -0300

    fix layout
    Signed-off-by: hendricksonweib <hendricksonrocha22@gmail.com>

--- src/CortesSession.jsx
+++ src/CortesSession.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
-import './css/CortesSession.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import CorteDadosFt from './CorteDadosFt';
+import Cortesimg from "../src/assets/Cortes.svg";
 import TitleCortesSession from './TitleCortesSession';
 
 function CortesSession() {
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col-md-6 col-xs-12">
-          <CorteDadosFt />
+    <div className="container-fluid">
+      <div className="row align-items-center">
+        <div className="col-md-6 col-xs-12 text-end">
+          <img className="img-fluid" src={Cortesimg} alt="Cortesimg" />
         </div>
         <div className="col-md-6 col-xs-12">
           <TitleCortesSession />
--- src/TitleCortesSession.jsx
+++ src/TitleCortesSession.jsx
@@ -1,15 +1,13 @@
-import React from 'react'
+import React from 'react';
 import './css/CortesSession.css';
 
 function TitleCortesSession() {
-    return (
-        <div>
-            <div className="navMainH2">
-                <h1 className="fontH2">Cresça com a estratégia <br />de conteúdo dos maiores <br />canais de entretenimento,<br /> notícias e educação!</h1>
-                <button className="btnH2">Aumente sua audiência</button>
-            </div>
-        </div>
-    )
+  return (
+    <div className="navMainH2">
+      <h1 className="fontH2">Cresça com a estratégia <br />de conteúdo dos maiores <br />canais de entretenimento,<br /> notícias e educação!</h1>
+      <button className="btnH2">Aumente sua audiência</button>
+    </div>
+  );
 }
 
-export default TitleCortesSession
+export default TitleCortesSession;
