516968f758e2dbe0b4a58ad7ded8b30dc2414315
hendricksonweib <hendricksonrocha22@gmail.com>
2024-03-27 19:31:43 -0300

    start the branch
    Signed-off-by: hendricksonweib <hendricksonrocha22@gmail.com>

---
+++ src/Dashboard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../src/css/Dashboard.css'
+
+function Dashboard() {
+  return (
+    <>
+    <div className='Dashboard-conteiner'>
+        
+    </div>
+    </>
+  )
+}
+
+export default Dashboard
---
+++ src/LandingPage.jsx
@@ -0,0 +1,32 @@
+import './App.css';
+import Header from './Header'
+import CortesSession from './CortesSession';
+import ProcessosSession from './ProcessosSession';
+import Who from './Who.jsx'; 
+import BigCase from './BigCase.jsx'
+import Formulario from './Formulario.jsx';
+import Parceiros from './Parceiros.jsx';
+import Accordion from './Accordion.jsx';
+import FooterSession from './FooterSession.jsx';
+import { Analytics } from "@vercel/analytics/react"
+
+function LandingPage() {
+  return (
+    <>
+      <Header/>
+      <CortesSession/>
+      <ProcessosSession/>
+      <Who/>
+      <BigCase/>
+      <div id="Formulario">
+        <Formulario />
+      </div>
+      <Parceiros/>
+      <Accordion/> 
+      <FooterSession/>
+      <Analytics />
+    </>
+  );
+}
+
+export default LandingPage;
